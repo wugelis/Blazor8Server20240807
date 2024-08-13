fb4e48276bb6a06290305648cba877d31c250853
Gelis <wergelis@msn.com>
2024-08-13 16:10:30 +0800

    完成第二天上課內容的範例程式

---
+++ Blazor8Server20240807/Components/Controls/MyButton.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor8Server20240807.Components.Controls
+{
+    public partial class MyButton
+    {
+        int count = 0;
+        [Parameter] public string? ButtonText { get; set; }
+        [Parameter] public string? ButtonNum { get; set; }
+
+        private string _buttonText = "我是按鈕";
+
+        async Task ChangeCount()
+        {
+            count++;
+
+            ButtonText = _buttonText;
+
+            string text = $"{ButtonText} {count}";
+
+            ButtonText = await Task.FromResult(text);
+        }
+
+        protected override bool ShouldRender()
+        {
+            switch(ButtonNum)
+            {
+                case "btn1":
+                    return true;
+                case "btn2":
+                    return false;
+            }
+            return false;
+        }
+    }
+}
---
+++ Blazor8Server20240807/Components/Controls/MyButton3.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Blazor8Server20240807.Components.Controls
+{
+    public class MyButton3: ComponentBase
+    {
+        [Parameter] public string? ButtonText { get; set; }
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.AddMarkupContent(0, $"<button class=\"btn btn-primary\">{ButtonText}</button");
+            base.BuildRenderTree(builder);
+        }
+    }
+}
